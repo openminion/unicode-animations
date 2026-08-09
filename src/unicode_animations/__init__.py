3e21fce4603363720546983f6e74655cb2281018
J <j@openminion.com>
2026-08-09 03:45:01 -0700

    build(release): prepare unicode-animatio 0.0.6

--- src/unicode_animations/__init__.py
+++ src/unicode_animations/__init__.py
@@ -1,6 +1,6 @@
 """Unicode and ASCII terminal animations for Python."""
 
-__version__ = "0.0.6rc1"
+__version__ = "0.0.6"
 
 from .braille import Spinner, grid_to_braille, make_grid
 from .catalog import (
